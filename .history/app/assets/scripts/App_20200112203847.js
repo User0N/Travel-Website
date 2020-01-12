a4b73fd81443b3d611dce2a2a558d1c9353344fa
Basam <basamahmad7@gmail.com>
2020-01-12 20:59:53 +1100

    Completed the reveal on scroll behaviour.

--- .history/app/assets/scripts/App_20200112203847.js
+++ .history/app/assets/scripts/App_20200112203847.js
@@ -1,7 +1,5 @@
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
-import $ from 'jquery';
 
 var mobileMenu = new MobileMenu();
-new RevealOnScroll($(".feature-item"), "85%");
-new RevealOnScroll($(".testimonial"), "60%");
+var revealOnScroll = new RevealOnScroll();
